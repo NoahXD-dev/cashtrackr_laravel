c20396d60180b4ff71630915d7ea788cf6ebf77a
Noe Ramirez Castro <ramcas.noe@gmail.com>
2026-05-23 17:56:29 -0600

    refactor(auth): traducir atributos y limpiar mensajes en SignupRequest

--- app/Http/Requests/SignupRequest.php
+++ app/Http/Requests/SignupRequest.php
@@ -8,29 +8,30 @@ use Illuminate\Validation\Rules\Password;
 
 class SignupRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
+    public function attributes(): array
     {
-        return true;
+        return [
+            'name' => 'nombre',
+            'email' => 'email',
+            'password' => 'contraseña'
+        ];
     }
 
     public function messages(): array
     {
         return [
-            'name.required' => 'El nombre es obligatorio',
-            'email.required' => 'El email es obligatorio',
-            'email.email' => 'El email debe ser una dirección de correo electrónico válida',
-            'email.unique' => 'El email ya está registrado',
-            'password.required' => 'La contraseña es obligatoria',
-            'password.confirmed' => 'Las contraseñas no coinciden',
-            'password.min' => 'La contraseña debe tener al menos :min caracteres',
-            'password.letters' => 'La contraseña debe contener al menos 1 letra',
-            'password.mixed' => 'La contraseña debe contener al menos 1 letra mayúscula y 1 letra minúscula',
-            'password.symbols' => 'La contraseña debe contener al menos 1 carácter especial',
-            'password.numbers' => 'La contraseña debe contener al menos 1 número',
-            'password.uncompromised' => 'La contraseña ha sido comprometida en una filtración de datos. Por favor, elige una contraseña diferente.',
+            'name.required' => 'El :attribute es obligatorio',
+            'email.required' => 'El :attribute es obligatorio',
+            'email.email' => 'El :attribute debe ser una dirección de correo electrónico válida',
+            'email.unique' => 'El :attribute ya está registrado',
+            'password.required' => 'El :attribute es obligatorio',
+            'password.confirmed' => 'Las :attributes no coinciden',
+            'password.min' => 'La :attribute debe tener al menos :min caracteres',
+            'password.letters' => 'La :attribute debe contener al menos 1 letra',
+            'password.mixed' => 'La :attribute debe contener al menos 1 letra mayúscula y 1 letra minúscula',
+            'password.symbols' => 'La :attribute debe contener al menos 1 carácter especial',
+            'password.numbers' => 'La :attribute debe contener al menos 1 número',
+            'password.uncompromised' => 'La :attribute ha sido comprometida en una filtración de datos. Por favor, elige una contraseña diferente.',
         ];
     }
 
