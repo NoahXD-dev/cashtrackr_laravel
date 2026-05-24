35bd8729d0ebbdb6b9f1ded5f96af9fb48f01786
Noe Ramirez Castro <ramcas.noe@gmail.com>
2026-05-24 12:06:19 -0600

    feat(views): crear vista de presupuestos, formulario y corregir nombres de secciones

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,10 +2,10 @@
 
 @section('content')
     <div class="max-w-5xl mx-auto p-5 lg:p-10">
-        @yield('accions')
+        @yield('actions')
     </div>
 
     <main class="mt-5 max-w-5xl mx-auto p-5 lg:p-10 mb-20">
-        @yield('dashboard-content')
+        @yield('dashboard-contents')
     </main>
 @endsection
